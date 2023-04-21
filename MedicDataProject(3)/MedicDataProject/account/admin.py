47ce77de9c4bcc45fdf885e7f4f7158dd9044c86
Fernandoelcrack <yayferyayfer9.com>
2023-04-20 20:28:51 -0600

    borrando

--- MedicDataProject(3)/MedicDataProject/account/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from account.models import *
-# Register your models here.
-
-admin.site.register(User),
-admin.site.register(ExpedienteG),
-admin.site.register(ExpedienteD),
-admin.site.register(ExpedienteO),
--- MedicDataProject(3)/MedicDataProject/account/static/script.js
+++
@@ -1,8 +0,0 @@
-let popup = document.getElementById("popup");
-
-function openPopup() {
-  popup.classList.add("exp-popup");
-}
-function closePopup() {
-  popup.classList.remove("exp-popup");
-}
