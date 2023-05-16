f84132225e8103dc12569146e8a43529539eb230
Fernandoelcrack <yayferyayfer9.com>
2023-05-16 17:58:30 -0600

    Terminado

--- MedicDataProject/MedicDataProject/settings.py
+++ MedicDataProject/MedicDataProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w611%fjul%*coz!bho!0z$=g3!)lvb4n=m$v74zju2^%3ayrrj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
