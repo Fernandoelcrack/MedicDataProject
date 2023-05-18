2bc3b9e644936ec9319e2f230493966a4284b925
Fernandoelcrack <yayferyayfer9.com>
2023-05-17 20:09:17 -0600

    viendo

--- MedicDataProject/MedicDataProject/settings.py
+++ MedicDataProject/MedicDataProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w611%fjul%*coz!bho!0z$=g3!)lvb4n=m$v74zju2^%3ayrrj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "http://omaya.gerdoc.com/"]
+ALLOWED_HOSTS = [ "*"]
 
 
 # Application definition
